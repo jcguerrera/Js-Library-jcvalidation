341e567f3d13185435eaf5fa24836624603b7672
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-07-05 16:51:02 -0500

    Test functionality fixed

--- validatorForm.test.js
+++
@@ -1,9 +0,0 @@
-// import {expresions} from "../app";
-
-import { expresions as _expresions } from './app.js';
-
-
-
-test('Regex', () => {
-    expect(_expresions.mail.test('younger@gmail.com')).toBe(true);
-});
